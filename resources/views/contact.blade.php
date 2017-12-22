dd653a2b4a2ec0da8a743fb01a3de0b2ebbbade9
Brad Roberts <brad@bradroberts.net>
2017-12-22 17:49:08 -0500

    Removed test form data

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -14,10 +14,10 @@
         <p>We'd love to hear from you. Let us know what questions you have or how we can make Movement better for you and your student ministry.</p>
 
         <form class="ContactForm" method="post">
-            <input type="text" name="name" placeholder="your name" value="Brad Roberts" required>
-            <input type="text" name="email" placeholder="email" value="bradr@faithpromise.org" required>
-            <input type="text" name="phone" placeholder="best phone # to reach you" value="(865) 973-2311">
-            <textarea name="body">Lorm ipsum means that its really importent for you to stay off drugs and stay in scool. You need to no things that will help you in life. Like MATHS and gym. You don't want to be dum.</textarea>
+            <input type="text" name="name" placeholder="your name" value="" required>
+            <input type="text" name="email" placeholder="email" value="" required>
+            <input type="text" name="phone" placeholder="best phone # to reach you" value="">
+            <textarea name="body"></textarea>
             <button type="submit">Send</button>
         </form>
 
