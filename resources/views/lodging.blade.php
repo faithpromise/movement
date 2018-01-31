3dc41d355c741a7078022452b310ddabff610f30
Brad Roberts <brad@bradroberts.net>
2018-01-31 16:21:13 -0500

    Styling

--- resources/views/lodging.blade.php
+++ resources/views/lodging.blade.php
@@ -4,10 +4,80 @@
 
     <h1>Lodging</h1>
 
-    <h2>F.A.Q.</h2>
+    <h2>Hotels</h2>
 
-    <h3>Are any special hotel rates available?</h3>
-    <p>Yes. You can find specialized rates with our partner hotels here (hyperlink).</p><!-- TODO: link -->
+    <h3>Hampton Inn &amp; Suites Turkey Creek/Farragut</h3>
+    <p>
+        <a href="https://goo.gl/maps/q58YQAeJ2HT2">11340 Campbell Lakes Drive, Knoxville, TN 37934</a>
+    </p>
+    <p>
+        <a href="tel:18659660303">(865) 966-0303</a>
+    </p>
+    <ul>
+        <li>Double room – $104/night</li>
+        <li>Continental breakfast</li>
+        <li>Group booking ID: 435384</li>
+        <li><a href="https://secure3.hilton.com/en_US/hp/reservation/book.htm?inputModule=HOTEL&ctyhocn=TYSTCHX&spec_plan=CHHMOV&arrival=20180711&departure=20180715&cid=OM,WW,HILTONLINK,EN,DirectLink&fromId=HILTONLINKDIRECT">Click here for group booking</a></li>
+        <li>Reservation cut-off date: June 20th, 2018</li>
+    </ul>
+
+    <hr>
+
+    <h3>Staybridge Suites – Knoxville West</h3>
+    <p>
+        <a href="https://goo.gl/maps/SzTahHqimuD2"></a>
+    </p>
+    <p>
+        <a href="tel:18656757829">(865) 675-7829</a>
+    </p>
+    <ul>
+        <li>King studio suite (1 bed, 1 pull-out couch) – $99/night</li>
+        <li>Queen studio suite (2 beds) – $99/night</li>
+        <li>1-bedroom king studio suites (1 bed, 1 pull-out couch) – $109/night</li>
+        <li>2-bedroom (1 king, 2 full) studio suites – $159/night</li>
+        <li>Continental breakfast</li>
+        <li>Group booking via phone or booking link with code: FPC</li>
+        <li><a href="https://www.staybridge.com/redirect?path=hd-hotel-room-rates&brandCode=SB&localeCode=es&regionCode=1&hotelCode=tysle&_PMID=99801505&GPC=FPC&viewfullsite=true">Click here for group booking</a> (Use code: FPC)</li>
+        <li>Reservation cut-off date: June 20th, 2018</li>
+    </ul>
+
+    <hr>
+
+    <h3>Courtyard by Marriott Knoxville Cedar Bluff</h3>
+    <p>
+        <a href="https://goo.gl/maps/MZXiKiHfHyQ2">216 Langley Pl, Knoxville, TN 37922</a>
+    </p>
+    <p>
+        <a href="tel:18655390600">(865) 539-0600</a>
+    </p>
+    <ul>
+        <li>Double room – $109/night (no breakfast)</li>
+        <li>Double room – $119/night (with breakfast)</li>
+        <li>*Option for breakfast</li>
+        <li>Reservation cut-off date: June 20th, 2018</li>
+        <li>Group booking ID: 435387</li>
+        {{--TODO: Add booking link--}}
+        {{--<li>Booking Link:</li>--}}
+    </ul>
+
+    <hr>
+
+    <h3>Home2 Suites by Hilton Knoxville West</h3>
+    <p>
+        <a href="https://goo.gl/maps/j5e5NPL2Gpu">380 N Peters Rd, Knoxville, TN 37922</a>
+    </p>
+    <p>
+        <a href="tel:18659739444">(865) 973-9444</a>
+    </p>
+    <ul>
+        <li>Double room – $129/night</li>
+        <li>Continental breakfast</li>
+        <li>Reservation cut-off date: June 20th, 2018</li>
+        <li>Group booking through online link with code FPC:</li>
+        <li><a href="http://home2suites.hilton.com/en/ht/groups/personalized/T/TYSNPHT-FPC-20180711/index.jhtml?WT.mc_id=POG">Click here for group booking</a></li>
+    </ul>
+
+    <h2>F.A.Q.</h2>
 
     <h3>Do Movement attendees have to stay at one of the hotels listed on the website?</h3>
     <p>No; however, Movement Conference has worked with our partner hotels to find the best hotel rates possible for registrants traveling from out of town.</p>
