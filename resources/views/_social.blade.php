241f7f4263304dd58b950ca9d82131a8a5180f45
Brad Roberts <brad@bradroberts.net>
2018-04-10 14:58:28 -0400

    Updated social info

--- resources/views/_social.blade.php
+++ resources/views/_social.blade.php
@@ -1,21 +1,21 @@
 {{--TODO: Links--}}
 <ul class="SocialList">
+    {{--<li class="SocialList-item">--}}
+        {{--<a class="SocialList-link" href="">--}}
+            {{--<svg role="img" class="SocialIcon">--}}
+                {{--<use xlink:href="#icon-facebook"></use>--}}
+            {{--</svg>--}}
+        {{--</a>--}}
+    {{--</li>--}}
+    {{--<li class="SocialList-item">--}}
+        {{--<a class="SocialList-link" href="">--}}
+            {{--<svg role="img" class="SocialIcon">--}}
+                {{--<use xlink:href="#icon-twitter"></use>--}}
+            {{--</svg>--}}
+        {{--</a>--}}
+    {{--</li>--}}
     <li class="SocialList-item">
-        <a class="SocialList-link" href="">
-            <svg role="img" class="SocialIcon">
-                <use xlink:href="#icon-facebook"></use>
-            </svg>
-        </a>
-    </li>
-    <li class="SocialList-item">
-        <a class="SocialList-link" href="">
-            <svg role="img" class="SocialIcon">
-                <use xlink:href="#icon-twitter"></use>
-            </svg>
-        </a>
-    </li>
-    <li class="SocialList-item">
-        <a class="SocialList-link" href="">
+        <a class="SocialList-link" href="https://www.instagram.com/movement_conference/">
             <svg role="img" class="SocialIcon">
                 <use xlink:href="#icon-instagram"></use>
             </svg>
