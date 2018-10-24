4e9d3b39e9c5940118eca0235742ca407933d9b7
Brad Roberts <brad@bradroberts.net>
2018-10-24 14:19:56 -0400

    Update event date

--- config/movement.php
+++ config/movement.php
@@ -3,6 +3,6 @@
 use Carbon\Carbon;
 
 return [
-    'conference_start_date' => Carbon::create(2018, 7, 12, 18, 30, 0, 'America/New_York'),
-    'conference_end_date'   => Carbon::create(2018, 7, 14, 17, 30, 0, 'America/New_York'),
+    'conference_start_date' => Carbon::create(2019, 7, 18, 18, 30, 0, 'America/New_York'),
+    'conference_end_date'   => Carbon::create(2019, 7, 20, 17, 30, 0, 'America/New_York'),
 ];
