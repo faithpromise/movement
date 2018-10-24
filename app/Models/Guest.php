3935d856ed366764942de6ce221f791a5fc75008
Brad Roberts <brad@bradroberts.net>
2018-10-24 17:08:41 -0400

    Add soft deletes to guest model

--- app/Models/Guest.php
+++ app/Models/Guest.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Guest extends Model {
+
+    use SoftDeletes;
 
     public function getImageAttribute() {
         return 'https://placehold.it/1920x1080';
